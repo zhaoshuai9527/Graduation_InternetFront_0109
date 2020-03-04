a715ce3b17f5b1bb65daf008344b380ba46aed59
zhaoshuai <5255802+zhaoshuai2019@user.noreply.gitee.com>
2020-03-04 23:59:54 +0800

    路由

---
+++ src/router.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from './components/Login.vue'
+import Register from './components/Register.vue'
+import Home from './components/Home.vue'
+import Welcome from './components/Welcome.vue'
+import User from './components/user/User.vue'
+import PublishTopic from './components/teacher/PublishTopic.vue'
+import TeacherSelectStudent from './components/teacher/TeacherSelectStudent.vue'
+import StudentSelectTopic from './components/student/StudentSelectTopic.vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', name: 'Login', component: Login },
+    { path: '/register', name: 'Register', component: Register },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        { path: '/welcome', name: 'Welcome', component: Welcome },
+        { path: '/user', name: 'User', component: User },
+        { path: '/publishTopic', name: 'PublishTopic', component: PublishTopic },
+        { path: '/studentSelectTopic', name: 'StudentSelectTopic', component: StudentSelectTopic },
+        { path: '/teacherSelectStudent', name: 'TeacherSelectStudent', component: TeacherSelectStudent }
+      ]
+    }
+  ]
+})
+
+// 防止用户直接访问/home页面
+// 挂在路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 从哪个路径跳转而来
+  // next（）放行   next（'/login'）强制跳转
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
+
+// 暴露router
+export default router
